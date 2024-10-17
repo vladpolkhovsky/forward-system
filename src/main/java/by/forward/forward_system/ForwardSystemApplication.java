940bf19fa9d66aeca894f7b041a1b530c617c57a
vpolkhovsky <vlad.polkhovsky@gmail.com>
2024-10-17 11:06:20 +0300

    Доработка

--- src/main/java/by/forward/forward_system/ForwardSystemApplication.java
+++ src/main/java/by/forward/forward_system/ForwardSystemApplication.java
@@ -1,5 +1,6 @@
 package by.forward.forward_system;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,9 +9,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class ForwardSystemApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Moscow"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ForwardSystemApplication.class, args);
