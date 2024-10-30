f4a5a78eaf42265924811967db58f748556fe399
vpolkhovsky <vlad.polkhovsky@gmail.com>
2024-10-30 11:43:57 +0300

    Доработка

--- src/main/java/by/forward/forward_system/core/jpa/repository/OrderRepository.java
+++ src/main/java/by/forward/forward_system/core/jpa/repository/OrderRepository.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
 
-    @Query(nativeQuery = true, value = "SELECT o.* from forward_system.orders o " +
+    @Query(nativeQuery = true, value = "SELECT distinct o.* from forward_system.orders o " +
         "left join forward_system.order_participants op on o.id = op.order_id " +
         "where op.user_id = :userId")
     List<OrderEntity> findOrdersWithUserInParticipant(Long userId);
