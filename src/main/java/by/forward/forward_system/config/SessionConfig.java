8dc44714af18104f7d3a01a880a3f5793a0f5ccb
vpolkhovsky <vlad.polkhovsky@gmail.com>
2024-11-07 20:05:06 +0300

    Добавление viewport

--- src/main/java/by/forward/forward_system/config/SessionConfig.java
+++ src/main/java/by/forward/forward_system/config/SessionConfig.java
@@ -1,7 +1,6 @@
 package by.forward.forward_system.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @Configuration
 //@EnableJdbcHttpSession(tableName = "forward_system.SPRING_SESSION", cleanupCron = "0 0 * * * *")
