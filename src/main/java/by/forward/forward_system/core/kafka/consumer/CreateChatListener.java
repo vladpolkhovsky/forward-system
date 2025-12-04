b28b2411305dc1665f627d9b00bf5a4e23b7e790
vladpolkhovsky <vlad.polkhovsky@gmail.com>
2025-12-04 21:40:02 +0300

    deploy-04.12.2025-1

--- src/main/java/by/forward/forward_system/core/kafka/consumer/CreateChatListener.java
+++ src/main/java/by/forward/forward_system/core/kafka/consumer/CreateChatListener.java
@@ -14,6 +14,7 @@ import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.kafka.retrytopic.DltStrategy;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -25,7 +26,8 @@ public class CreateChatListener {
     private final MessageService messageService;
 
     @RetryableTopic(
-        attempts = "2",
+        attempts = "5",
+        backoff = @Backoff(value = 7000L, multiplier = 2),
         kafkaTemplate = "createChatEventKafkaTemplate",
         exclude = {DataIntegrityViolationException.class}
     )
@@ -40,7 +42,9 @@ public class CreateChatListener {
             ack.acknowledge();
         } catch (DataIntegrityViolationException e) {
             // Бизнес-ошибки - подтверждаем сообщение, не повторяем
-            log.error("Business error processing chat creation event: {}. Error: {}", event, e.getMessage());
+            log.error("Business error processing chat creation event: {}. Error: {}", event, e.getMessage(), e);
+            messageService.sendMessageToErrorChat("ERROR: Ошибка обработки задания на создание чата: " + event
+                + " Error: " + ExceptionUtils.getStackTrace(e));
             ack.acknowledge();
         } catch (Exception e) {
             // Технические ошибки - пробрасываем для retry
@@ -53,7 +57,7 @@ public class CreateChatListener {
     public void listenDlt(@Payload CreateChatEvent event, Acknowledgment ack) {
         try {
             log.error("Event that failed processing after retries: {}", event);
-            messageService.sendMessageToErrorChat("Ошибка обработки задания на создание чата: " + event);
+            messageService.sendMessageToErrorChat("DLT: Ошибка обработки задания на создание чата: " + event);
         } catch (Exception e) {
             log.error("Technical error processing event: {}", event, e);
         }
