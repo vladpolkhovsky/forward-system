d930b6d359ceccc2fdc0ed684ea6ea937d2e3ebd
vladpolkhovsky <vlad.polkhovsky@gmail.com>
2026-08-23 15:35:42 +0300

    deploy-23.08.2026-01

--- src/main/java/by/forward/forward_system/core/services/NewOrderService.java
+++ src/main/java/by/forward/forward_system/core/services/NewOrderService.java
@@ -26,10 +26,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
